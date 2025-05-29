66f4b5818f8634f04b033081f1a92a5197d42c49
juliarta99 <adij4255@gmail.com>
2025-05-29 23:28:51 +0800

    fix: competition table

--- database/migrations/2025_05_17_111811_create_competitions_table.php
+++ database/migrations/2025_05_17_111811_create_competitions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 50);
             $table->string('slug', 50)->unique();
+            $table->string('code', 6);
             $table->string('icon_competition');
             $table->string('degree', 25);
             $table->double('wave_1_price');

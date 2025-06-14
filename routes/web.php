40d6a85fb9f1bb15ebb2042f4d3825c40a4d5cd4
GoesDey <gusdekmh7778@gmail.com>
2025-06-14 22:32:21 +0800

    fix: change route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/livewire', function () {
-    return view('livewire.register');
+Route::get('/register', function () {
+    return view('register');
 });

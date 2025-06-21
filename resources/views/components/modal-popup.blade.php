288d32dd7b882130abadc223f8da9fbd5f2f9dad
GoesDey <gusdekmh7778@gmail.com>
2025-06-21 23:16:44 +0800

    fix: make modal component

---
+++ resources/views/components/modal-popup.blade.php
@@ -0,0 +1,29 @@
+<div>
+    @props([
+    'id',
+    'title',
+    'message',
+    'type' => 'accept', // default: accept | alternatif: reject
+    'confirmFunction',
+    'cancelFunction'
+])
+
+    <div id="{{ $id }}" class="fixed inset-0 flex items-center justify-center w-screen h-screen backdrop-blur-sm hidden z-50">
+        <div class="bg-white p-6 rounded-lg shadow-xl w-90">
+            <h2 class="text-lg font-semibold mb-4">{{ $title }}</h2>
+            <p class="mb-4">{{ $message }}</p>
+
+            @if($type === 'reject')
+                <textarea id="reject-note" class="w-full border rounded p-2 mb-6 resize-none" rows="3" placeholder="Ketik catatan untuk user... *"></textarea>
+            @endif
+
+            <div class="flex justify-end gap-4">
+                <button onclick="{{ $cancelFunction }}" class="bg-gray-700 text-white px-6 py-1 rounded hover:bg-gray-800">Batal</button>
+                <button onclick="{{ $confirmFunction }}"
+                    class="{{ $type === 'reject' ? 'bg-red-600 hover:bg-red-700' : 'bg-[#029161] hover:bg-green-700' }} text-white px-6 py-1 rounded">
+                    Ya
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/manajemen-peserta.blade.php
+++ resources/views/manajemen-peserta.blade.php
@@ -3,34 +3,13 @@
     <div class="bg-gray-100 w-full h-screen font-jakarta">
           <div class="flex justify-between items-center mb-8">
                <h2 class="text-4xl font-bold text-[#4C4C4C]">Manajemen User</h2>
-               <button class="flex items-center justify-between gap-2 bg-[#00C482] rounded-lg px-5 py-2 cursor-pointer">
+               <button class="flex items-center justify-between gap-2 bg-[#00C482] hover:bg-[#16ad7a] rounded-lg px-5 py-2 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
                          <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                     </svg>
                     <p class="text-white">Export</p>
                </button>
           </div>
-          {{-- <div class="flex bg-white rounded-md gap-4 items-center px-8 py-6 mx-8 mb-8 shadow-xl">
-               <div class="grow relative">
-                    <input class=" border rounded-md px-8 py-2 w-full" type="text" placeholder="Telusuri User">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="absolute top-2 left-1 size-6">
-                         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                    </svg>
-               </div>
-               <select class="bg-[#007BFF] rounded-lg px-5 py-3 cursor-pointer">
-                    <div class="flex items-center justify-between gap-2 ">
-                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
-                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                         </svg>
-                         
-                    </div>
-                    <option value="">Filter</option>
-                    <option value="">Diterima</option>
-                    <option value="">Ditolak</option>
-                    <option value="">Menunggu</option>
-                    <option value="">Kompetisi</option>
-               </select>
-          </div> --}}
           <div class="flex bg-white rounded-md gap-4 items-center px-8 py-6 mb-8 shadow-xl">
           <div class="grow relative">
                <input name="search" value="{{ request('search') }}" class="border rounded-md px-8 py-2 w-full"  type="text"  placeholder="Telusuri User...">
@@ -43,7 +22,7 @@
                           5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                </svg>
           </div>
-          <button type="submit" class="flex items-center justify-between gap-2 bg-[#007BFF] rounded-lg px-5 py-3 cursor-pointer">
+          <button type="submit" class="flex items-center justify-between gap-2 bg-[#007BFF] hover:bg-[#0062ff] rounded-lg px-5 py-3 cursor-pointer">
                <svg xmlns="http://www.w3.org/2000/svg" fill="none" 
                     viewBox="0 0 24 24" stroke-width="1.5" 
                     stroke="white" class="size-6">
@@ -65,11 +44,7 @@
                     </thead>
                     <tbody class="bg-white">
                          @foreach ($users as $user)
-                              <tr id="user-row-{{ $user['id'] }}" class="
-                                   {{ $user['status'] === 'Menunggu' ? 'bg-[#FFD9001A]' : '' }}
-                                   {{-- {{ $user['status'] === 'Ditolak' ? 'bg-[#FF00001A]' : '' }}
-                                   {{ $user['status'] === 'Diterima' ? 'bg-white' : '' }} --}}
-                              ">
+                              <tr id="user-row-{{ $user['id'] }}" class="bg-[#FFD9001A]">
                                    <td class="text-center px-4 py-5">{{ $user['no_reg'] }}</td>
                                    <td class="px-4 py-5">{{ $user['nama'] }}</td>
                                    <td class="text-center px-4 py-5">{{ $user['nisn'] }}</td>
@@ -78,60 +53,49 @@
                                    <td class="text-center px-4 py-5">{{ $user['kompetisi'] }}</td>
                                    <td id="user-status-{{ $user['id'] }}" class="text-center px-4 py-5">{{ $user['status'] }}</td>
                                    <td class="px-4 py-5 flex items-center justify-evenly">
-
-                                   <button 
-                                        class="btn-accept cursor-pointer " 
-                                        data-user-id="{{ $user['id'] }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#029161" class="size-8">
-                                             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm3.844-8.791a.75.75 0 0 0-1.188-.918l-3.7 4.79-1.649-1.833a.75.75 0 1 0-1.114 1.004l2.25 2.5a.75.75 0 0 0 1.15-.043l4.25-5.5Z" clip-rule="evenodd" />
-                                        </svg>
-                                   </button>
-                                   <button 
-                                        class="btn-reject cursor-pointer {{ $user['status'] === 'Ditolak' || $user['status'] === 'Diterima'  ? 'invisible' : '' }}" 
-                                        data-user-id="{{ $user['id'] }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="size-8">
-                                             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm2.78-4.22a.75.75 0 0 1-1.06 0L8 9.06l-1.72 1.72a.75.75 0 1 1-1.06-1.06L6.94 8 5.22 6.28a.75.75 0 0 1 1.06-1.06L8 6.94l1.72-1.72a.75.75 0 1 1 1.06 1.06L9.06 8l1.72 1.72a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd" />
-                                        </svg>
-                                   </button>
-                                   <button class="cursor-pointer">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#007BFF" class="size-10">
-                                             <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" />
-                                             <path fill-rule="evenodd" d="M1.38 8.28a.87.87 0 0 1 0-.566 7.003 7.003 0 0 1 13.238.006.87.87 0 0 1 0 .566A7.003 7.003 0 0 1 1.379 8.28ZM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" clip-rule="evenodd" />
-                                        </svg>    
-                                   </button>
+                                        <button 
+                                             class="btn-accept cursor-pointer " 
+                                             data-user-id="{{ $user['id'] }}">
+                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#029161" class="size-8">
+                                                  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm3.844-8.791a.75.75 0 0 0-1.188-.918l-3.7 4.79-1.649-1.833a.75.75 0 1 0-1.114 1.004l2.25 2.5a.75.75 0 0 0 1.15-.043l4.25-5.5Z" clip-rule="evenodd" />
+                                             </svg>
+                                        </button>
+                                        <button 
+                                             class="btn-reject cursor-pointer " 
+                                             data-user-id="{{ $user['id'] }}">
+                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="size-8">
+                                                  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm2.78-4.22a.75.75 0 0 1-1.06 0L8 9.06l-1.72 1.72a.75.75 0 1 1-1.06-1.06L6.94 8 5.22 6.28a.75.75 0 0 1 1.06-1.06L8 6.94l1.72-1.72a.75.75 0 1 1 1.06 1.06L9.06 8l1.72 1.72a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd" />
+                                             </svg>
+                                        </button>
+                                        <button class="cursor-pointer">
+                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#007BFF" class="size-10">
+                                                  <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" />
+                                                  <path fill-rule="evenodd" d="M1.38 8.28a.87.87 0 0 1 0-.566 7.003 7.003 0 0 1 13.238.006.87.87 0 0 1 0 .566A7.003 7.003 0 0 1 1.379 8.28ZM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" clip-rule="evenodd" />
+                                             </svg>    
+                                        </button>
                                    </td>
                               </tr>
                          @endforeach
                     </tbody>
                </table>
           </div>
-
-
-     </div>
-     {{-- accept --}}
-     <div id="popup-confirm" class="fixed inset-0 flex items-center justify-center bg-black/50 hidden z-50">
-          <div class="bg-white p-6 rounded-lg shadow-xl w-96">
-               <h2 class="text-lg font-semibold mb-4">Konfirmasi Accept</h2>
-               <p class="mb-16">Anda yakin ingin memverifikasi data user ini?</p>
-               <div class="flex justify-end gap-4">
-                    <button onclick="closePopup()" class="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800">Batal</button>
-                    <button onclick="confirmAccept()" class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">Ya</button>
-               </div>
-          </div>
-     </div>
-     {{-- reject --}}
-     <div id="popup-reject" class="fixed inset-0 flex items-center justify-center bg-black/50 hidden z-50">
-          <div class="bg-white p-6 rounded-lg shadow-xl w-96">
-               <h2 class="text-lg font-bold mb-4">Konfirmasi Reject</h2>
-               <p class="mb-4">Anda yakin ingin menolak data user ini?</p>
-               <textarea id="reject-note" class="w-full border rounded p-2 mb-6 resize-none" rows="5"  placeholder="Ketik catatan untuk user... *"></textarea>
-               <div class="flex justify-end gap-4">
-                    <button onclick="closeRejectPopup()" class="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800">Batal</button>
-                    <button onclick="confirmReject()" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">Ya</button>
-               </div>
-          </div>
      </div>
 
+     <x-modal-popup 
+          id="popup-confirm"
+          title="Konfirmasi Accept"
+          message="Anda yakin ingin memverifikasi data user ini?"
+          confirmFunction="confirmAccept()"
+          cancelFunction="closePopup()" 
+     />
+     <x-modal-popup 
+          id="popup-reject"
+          title="Konfirmasi Reject"
+          message="Anda yakin ingin menolak data user ini?"
+          type="reject"
+          confirmFunction="confirmReject()"
+          cancelFunction="closeRejectPopup()" 
+     />
 </div>
 
 <script>

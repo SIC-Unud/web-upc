2068c0b8bddd03ae83f61fa96517870e6a684302
aurell-ev <tinklerel@gmail.com>
2025-06-27 01:21:18 +0800

    stye: fix mobile ui

--- resources/views/informasi-admin.blade.php
+++ resources/views/informasi-admin.blade.php
@@ -7,7 +7,7 @@
 
     <div class="flex justify-between items-center py-8">
         <h1 class="text-3xl md:text-5xl font-bold text-[#4C4C4C]">Informasi</h1>
-        <button @click="formOpen = true; modalTitle=''; modalContent=''; editMode=false" class="bg-[#007BFF] hover:shadow-xl text-white font-bold py-2 px-4 text-xs md:text-base">
+        <button @click="formOpen = true; modalTitle=''; modalContent=''; editMode=false" class="bg-[#007BFF] hover:shadow-xl text-white font-bold py-2 px-4 text-[10px] md:text-base">
             + Tambah
         </button>
     </div>
@@ -15,9 +15,9 @@
     <div class="space-y-4">
         @foreach ($informasi as $info)
             <div class="bg-white shadow-xl rounded-xl p-4 relative max-h-[140px] text-justify">
-                <h2 class="md:text-lg text-base font-bold text-gray-800 mb-1">{{ $info['title'] }}</h2>
+                <h2 class="md:text-lg text-sm font-bold text-gray-800 mb-1">{{ $info['title'] }}</h2>
 
-                <div class="text-xs md:text-base text-black relative overflow-hidden">
+                <div class="text-[10px] md:text-base text-black relative overflow-hidden">
                     @php
                         $limitDesktop = 210;
                         $limitMobile = 80;
@@ -25,7 +25,7 @@
                         $previewMobile = Str::limit($info['content'], $limitMobile);
                     @endphp
 
-                    <p class="text-xs md:text-base text-black">
+                    <p class="text-[10px] md:text-base text-black">
                         <span x-show="!isMobile">{!! nl2br(e($previewDesktop)) !!}</span>
                         <span x-show="isMobile">{!! nl2br(e($previewMobile)) !!}</span>
                         <span class="text-green-500 cursor-pointer hover:underline" @click="formOpen = true; modalTitle = `{{ addslashes($info['title']) }}`; modalContent = `{{ addslashes($info['content']) }}`; editMode = true">
@@ -52,18 +52,18 @@
                 <form @submit.prevent class="space-y-4">
                     <div>
                         <input type="text" name="title"
-                            class="mt-1 block w-full rounded-md border-black shadow-sm p-3 font-bold text-lg md:text-2xl text-black"
+                            class="mt-1 block w-full rounded-md border-black shadow-xs p-3 font-bold text-base md:text-2xl text-black"
                             placeholder="Masukkan judul..."
                             x-model="modalTitle">
                     </div>
                     <div>
                         <textarea name="content" rows="4"
-                            class="mt-1 block w-full rounded-md  border-black shadow-sm text-sm md:text-lg text-black text-justify overflow-y-auto min-h-[210px] p-3 pr-5"
+                            class="mt-1 block w-full rounded-md  border-black shadow-sm text-xs md:text-lg text-black text-justify overflow-y-auto min-h-[210px] p-3 pr-5"
                             placeholder="Tulis isi informasi..."
                             x-model="modalContent"></textarea>
                     </div>
                     <div class="text-right">
-                        <button type="button" class="bg-[#029161] hover:shadow-xl text-white font-bold py-2 px-6"
+                        <button type="button" class="bg-[#029161] hover:shadow-xl text-white font-bold text-xs md:text-base py-2 px-6"
                             @click="formOpen = false; editMode = false">
                             Simpan
                         </button>

c6df3e398186caf5b836e1b0581ea5a86cc69475
aurell-ev <tinklerel@gmail.com>
2025-08-30 08:38:01 +0800

    fix: fixing bug on modal

--- resources/views/cbt-admin.blade.php
+++ resources/views/cbt-admin.blade.php
@@ -8,7 +8,7 @@
                     <p x-show="showNotif" x-transition class="text-sm lg:text-base text-[#00C482] font-semibold">
                          Progres Berhasil Tersimpan!
                     </p>
-                    <button x-on:click="showConfirm = true" @click="isSaved = isSaving = true; showNotif = true; setTimeout(() => { showNotif = false; isSaving = false, isDirty = false }, 3000)" :class="isSaving ? 'bg-gray-400 cursor-not-allowed' : 'bg-[#00C482]'" class="bg-[#00C482] rounded-md lg:px-6 px-3 lg:py-3 py-1.5 cursor-pointer flex items-center gap-2">
+                    <button x-on:click="isDirty = false" @click="isSaved = isSaving = true; showNotif = true; setTimeout(() => { showNotif = false; isSaving = false}, 3000)" :class="isSaving ? 'bg-gray-400 cursor-not-allowed' : 'bg-[#00C482]'" class="bg-[#00C482] rounded-md lg:px-6 px-3 lg:py-3 py-1.5 cursor-pointer flex items-center gap-2">
                          <svg xmlns="http://www.w3.org/2000/svg" class="size-4 lg:size-5" fill="white" viewBox="0 0 16 16">
                               <path d="M11 2H9v3h2z"/>
                               <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"/>

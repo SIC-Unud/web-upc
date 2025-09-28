9f9b410d330379bf244d3ff02e0799821fe350fc
juliarta99 <adij4255@gmail.com>
2025-09-28 10:59:22 +0800

    fix: duration ban

--- app/Livewire/ParticipantCompetition.php
+++ app/Livewire/ParticipantCompetition.php
@@ -55,7 +55,7 @@ class ParticipantCompetition extends Component
     {
         ForbiddenUser::updateOrCreate(
             ['user_id' => Auth::user()->id],
-            ['expired_at' => now()->addMinutes(3)]
+            ['expired_at' => now()->addMinutes(1)]
         );
 
         if($this->competition->is_simulation) {
